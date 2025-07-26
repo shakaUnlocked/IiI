da154476e8eb7026dd4364abc431759518d71c44
shk <rhiannew77@gmail.com>
2025-07-26 20:52:55 -0300

    Create IdkHub.lua

---
+++ IdkHub.lua
@@ -0,0 +1,41 @@
+local ID = game.PlaceId
+local baseURL = "https://raw.githubusercontent.com/Zayn31214/name/refs/heads/main/"
+local Players = game:GetService("Players")
+local plr = Players.LocalPlayer
+
+function GetGame()
+    if ID == 15014439457 then
+        return "db.lua"
+    else
+        print("Game is not supported.")
+        return nil
+    end
+end
+
+local gameScript = GetGame()
+
+if gameScript then
+    loadstring(game:HttpGet(baseURL .. gameScript))()
+end
+
+for _, v in next, getconnections(plr.Idled) do
+    v:Disable()
+end
+
+local VirtualUser = game:GetService("VirtualUser")
+local status = getgenv().afk_toggle
+if status == nil then
+    getgenv().afk_toggle = false
+end
+
+if not plr then
+    error("Failed to get LocalPlayer reference")
+end
+
+plr.Idled:Connect(function()
+    if not getgenv().afk_toggle then return end
+    pcall(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
+end)
